4c91f46a5b74e940aadf080874fb9bbd982aafd0
Rodrigo Nascimento <rodrigo.nts@globo.com>
2020-05-16 17:37:16 -0300

    Updates

---
+++ precache-manifest.3a184583796e85c53d248e5e5413b712.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f916e40b4abf64e227df813dc870a35",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e68d00b0012838e43c04",
+    "url": "/static/css/main.b4816b7f.chunk.css"
+  },
+  {
+    "revision": "f40c8586d8f4976863db",
+    "url": "/static/js/2.89dc6f82.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.89dc6f82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e68d00b0012838e43c04",
+    "url": "/static/js/main.495f4e4b.chunk.js"
+  },
+  {
+    "revision": "c1a5be5950e49d70d1fd",
+    "url": "/static/js/runtime-main.74699e95.js"
+  },
+  {
+    "revision": "4674256cccbe34ecfc955010a0de3031",
+    "url": "/static/media/amazon_prime_video_logo.4674256c.svg"
+  },
+  {
+    "revision": "c7bc32fd6b6f76ff982bb0e2c28bc645",
+    "url": "/static/media/globoplay_logo.c7bc32fd.svg"
+  },
+  {
+    "revision": "f8e0ad67128b3041f730e2b3d08f7f1e",
+    "url": "/static/media/hbo_go_logo.f8e0ad67.svg"
+  },
+  {
+    "revision": "6dbba458959d4ce1edd2f5b3ab3ae13b",
+    "url": "/static/media/netflix_logo.6dbba458.svg"
+  }
+]);
